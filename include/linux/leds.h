25a973828828bf25633d05c21de4e03e89d50803
baddar90 <mnbaddar1990@gmail.com>
2020-01-13 09:36:35 +0000

    Revert "Add Headers modifications"
    This reverts commit 2dbdd24e37762056e63e180118d89eefce89d8b5.

--- include/linux/leds.h
+++ include/linux/leds.h
@@ -36,7 +36,6 @@ struct led_classdev {
 	enum led_brightness	 max_brightness;
 	enum led_brightness	 usr_brightness_req;
 	int			 flags;
-	int  blink_value;/*zte_led*/
 
 	/* Lower 16 bits reflect status */
 #define LED_SUSPENDED		(1 << 0)
